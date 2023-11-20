45e9306046d07494f59ed707413a101b62a6b0ad
MatejDziaba <matejdziaba@gmail.com>
2023-11-20 15:05:31 +0100

    Pridanie Favicon, ciest

--- src/run/app-routing.module.ts
+++ src/run/app-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomecomponentComponent } from '../app/homecomponent/homecomponent.component';
+import { CatalogcomponentComponent } from '../app/Catalog/catalogcomponent/catalogcomponent.component';
+import { BAdminComponent } from '../app/Catalog/b-admin/b-admin.component';
+import { ModuleAddComponent } from '../app/Catalog/module-add/module-add.component';
 
 const routes: Routes = [
-  {path: '',component: HomecomponentComponent}
+  {path: '', component: HomecomponentComponent},
+  {path: 'homecomponent', component: HomecomponentComponent},
+  {path: 'catalogcomponent', component: CatalogcomponentComponent},
+  {path: 'b-admin', component: BAdminComponent},
+  {path: 'module-add', component: ModuleAddComponent}
 ];
 
 @NgModule({
